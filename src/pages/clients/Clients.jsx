9a649e10e12dc0ac78a50060caabb23b42cd8044
Tiin24 <86517179+Tiin24@users.noreply.github.com>
2024-08-10 03:21:10 -0300

    Update Clients.jsx

--- src/pages/clients/Clients.jsx
+++ src/pages/clients/Clients.jsx
@@ -1,19 +1,14 @@
-import { useAuth0 } from '@auth0/auth0-react';
+
 import TablaClient from '../../components/Table/TablaClient';
 
 const Clients = () => {
-  const { isAuthenticated } = useAuth0();
+  
   return (
-    (isAuthenticated && (
+   
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Clientes </h1>
       <TablaClient />
     </div>
-  )) || (
-    <div className="flex justify-center items-center ">
-      <h2>Error: Debes logiarte para ingresar a este sitio</h2>
-    </div>
-  )
 );
 };
 
