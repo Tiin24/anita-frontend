6f8bd03e57cd66fb5048358681d8b7f75e9012be
Tiin24 <86517179+Tiin24@users.noreply.github.com>
2024-08-10 03:19:12 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 // import { useAuth0 } from "@auth0/auth0-react";
-import { useNavigate } from "react-router-dom";
 import Peluqueria from "../../assets/Peluqueria.jpg"
+import { Link } from "react-router-dom";
 
 export default function Home() {
   // const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -22,7 +22,9 @@ export default function Home() {
       <div className="w-1/2 flex flex-col items-center justify-center gap-24">
         <h1 className="font-poppins text-black text-3xl">Dashboard Peluqueria Anita</h1>
         <button type="button" onClick={navigate("/citas")} className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          <p className="font-poppins text-black">Get Started </p>
+  <Link to="/">
+<p className="font-poppins text-black">Get Started </p>
+</Link>          
         </button>
       </div>
     </div>
