5d6bd03c6f8c402a5a73c807b6575baffd8d0ab0
Tiin24 <86517179+Tiin24@users.noreply.github.com>
2024-08-10 03:21:45 -0300

    Update NuevoCliente.jsx

--- src/pages/clients/NuevoCliente.jsx
+++ src/pages/clients/NuevoCliente.jsx
@@ -1,18 +1,12 @@
-import { useAuth0 } from "@auth0/auth0-react";
 
 const NuevoCliente = () => {
-  const { isAuthenticated } = useAuth0();
+  
   return (
-    (isAuthenticated && (
+   
       <div className="p-4">
         <h1 className="text-2xl font-bold">Nuevo Cliente Page</h1>
         <p>Welcome to the Clients page!</p>
       </div>
-    )) || (
-      <div className="flex justify-center items-center ">
-        <h2>Error: Debes logiarte para ingresar a este sitio</h2>
-      </div>
-    )
   );
 };
 
