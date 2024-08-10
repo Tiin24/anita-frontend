ad57119b8bbb81154a5284c5efbeb33f9eee7b3b
Tiin24 <86517179+Tiin24@users.noreply.github.com>
2024-08-10 03:15:46 -0300

    Update Home.jsx
    saque aut0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,18 @@
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import Peluqueria from "../../assets/Peluqueria.jpg"
 
 export default function Home() {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  // const { loginWithRedirect, isAuthenticated } = useAuth0();
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    loginWithRedirect();
-  };
+  // const handleLogin = () => {
+  //   loginWithRedirect();
+  // };
 
-  if (isAuthenticated) {
-    navigate("/citas");
-  }
+  // if (isAuthenticated) {
+  //   navigate("/citas");
+  // }
 
   return (
     <div className="h-screen w-screen flex">
@@ -21,7 +21,7 @@ export default function Home() {
       </div>
       <div className="w-1/2 flex flex-col items-center justify-center gap-24">
         <h1 className="font-poppins text-black text-3xl">Dashboard Peluqueria Anita</h1>
-        <button type="button" onClick={handleLogin} className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <button type="button" onClick={navigate("/citas")} className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           <p className="font-poppins text-black">Get Started </p>
         </button>
       </div>
